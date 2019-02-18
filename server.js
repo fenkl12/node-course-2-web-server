dab2d4af914d5a6c8159f38dc999aca468265551
fenkl12 <31940787+fenkl12@users.noreply.github.com>
2019-02-18 16:36:20 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000
+
 var app = express()
 
 hbs.registerPartials(__dirname+ '/views/partials')
@@ -72,4 +74,4 @@ app.get('/bad',(req,res)=>{
         ErrorMessage: "Note working"
     })
 })
-app.listen(3000,()=>{console.log("Server is Up");})
+app.listen(port,()=>{console.log(`Server is up on ${port}`)})
