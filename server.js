6d3a57291a2b1f3d7ab4b54163c0218edef673d2
fenkl12 <31940787+fenkl12@users.noreply.github.com>
2019-02-18 16:50:34 -0500

    Addition of project page

--- server.js
+++ server.js
@@ -69,6 +69,14 @@ app.get('/about',(req,res)=>{
     })
 })
 
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle: "Projects Page",
+    })
+})
+
+
+
 app.get('/bad',(req,res)=>{
     res.send({
         ErrorMessage: "Note working"
