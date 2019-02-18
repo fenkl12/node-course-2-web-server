cf00b1882755d3f28c323a1e2d6c73ccafd788fd
fenkl12 <31940787+fenkl12@users.noreply.github.com>
2019-02-18 15:49:55 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express()
+
+hbs.registerPartials(__dirname+ '/views/partials')
+app.set('view engine','hbs')
+
+
+app.use((req,res,next)=> {
+
+    var now = new Date().toString()
+    var logUserInput =`${now}: ${req.method} ${req.url}`
+
+    console.log(logUserInput);
+    fs.appendFile('server.log',logUserInput +'\n', (err)=>{
+        if (err){
+            console.log("Unable to append server.log");
+        }
+    })
+    next()
+})
+
+//////////////// to get a front maintenace page. As it is the first webpage request on the script it will block all other pages. if there is no link in the page, next() does not matter 
+
+// app.use((req,res,next)=> {
+//     res.render('maintenace.hbs')
+  
+// })
+
+app.use(express.static(__dirname+ '/public'))
+
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear()
+    // return "test"
+})
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase()
+})
+
+// app.get('/',(req, res)=>{
+    
+//     // res.send('<h1>Hello Express</h1>'
+//     res.send({
+//         name: 'Fenkil',
+//         likes:["Hockey","Soccer" ]
+//     })
+
+// })
+
+app.get('/',(req, res)=>{
+    
+    res.render('home.hbs',{
+        pageTitle: "Home Page",
+        welcomeMessage: "Welcome to my Website"
+    })
+
+})
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle: "About Page",
+    })
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        ErrorMessage: "Note working"
+    })
+})
+app.listen(3000,()=>{console.log("Server is Up");})
